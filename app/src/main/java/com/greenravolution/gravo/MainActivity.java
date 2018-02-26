f829472aae6c85eda7c6a7eee242bb76ed1baf54
Bryan <bryanlow987@gmail.com>
2018-02-26 22:28:54 +0800

    replaced previosu layout with login page

--- app/src/main/java/com/greenravolution/gravo/MainActivity.java
+++ app/src/main/java/com/greenravolution/gravo/MainActivity.java
@@ -1,20 +1,32 @@
 package com.greenravolution.gravo;
 
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+    private static final Integer[] IMAGES= {R.drawable.rc1,R.drawable.rc2};
+    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        init();
 
     }
     @Override
@@ -34,4 +46,34 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    private void init() {
+        for(int i=0;i<IMAGES.length;i++)
+            ImagesArray.add(IMAGES[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new LoginPagerAdapter(MainActivity.this,ImagesArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == IMAGES.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 5000, 50000);
+    }
+
+
 }
