ccb14dd0ffe46793bf90770d6dfa397f17e43f0f
Bryan <bryanlow987@gmail.com>
2018-03-01 15:34:29 +0800

    added two colors and one image resource

--- app/src/main/java/com/greenravolution/gravo/Login/LoginActivity.java
+++ app/src/main/java/com/greenravolution/gravo/Login/LoginActivity.java
@@ -1,15 +1,15 @@
 package com.greenravolution.gravo.Login;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.Button;
 
 import com.greenravolution.gravo.Adapters.LoginPagerAdapter;
 import com.greenravolution.gravo.R;
@@ -22,24 +22,44 @@ import me.relex.circleindicator.CircleIndicator;
 
 
 public class LoginActivity extends AppCompatActivity {
-
-
     private static final Integer[] IMAGES = {R.drawable.login_rc_1, R.drawable.login_rc_2, R.drawable.login_rc_3};
     private static ViewPager mPager;
     private static int currentPage = 0;
+    Button login, register;
     private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
         init();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
+        login = findViewById(R.id.btnLogin);
+        register = findViewById(R.id.btnRegister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMain = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(toMain);
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(toRegister);
+                finish();
+            }
+        });
+
+
 
     }
 
