76a203a60fbf53ddd231b5f517a24438cf9d65f0
Bryan Low <bryanlow987@gmail.com>
2018-04-21 14:45:07 +0800

    finish up front end with minor items needed to change. Need to ask for Allan opinion on the app

---
+++ app/src/main/java/com/greenravolution/gravo/contents/ActivitySettingsItems.java
@@ -0,0 +1,13 @@
+package com.greenravolution.gravo.contents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySettingsItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_items);
+    }
+}
---
+++ app/src/main/java/com/greenravolution/gravo/contents/ActivityWebView.java
@@ -0,0 +1,13 @@
+package com.greenravolution.gravo.contents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
